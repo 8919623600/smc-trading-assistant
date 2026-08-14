14f796b32053d52f89591e35564bb3806326da25
Jyoti <ilu29jan@gmail.com>
2026-08-15 01:03:16 +0530

    code updated

--- smc/order_blocks.py
+++ smc/order_blocks.py
@@ -224,14 +224,14 @@ class OrderBlockEngine:
                 # Find bearish OB origin
                 # Last bullish candle before bearish displacement
 
-                for i in range(
+        for i in range(
                     bos_index - 1,
                     max(
                         bos_index - 50,
                         1
                     ),
                     -1
-                ):
+        ):
 
                     candle = self.df.iloc[i]
 
