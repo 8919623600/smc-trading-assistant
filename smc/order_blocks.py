9b3717b6efddb72d7a2c5d6f69bc0de2d68e14d9
Jyoti <ilu29jan@gmail.com>
2026-08-15 01:30:23 +0530

    code updated

--- smc/order_blocks.py
+++ smc/order_blocks.py
@@ -423,6 +423,12 @@ class OrderBlockEngine:
         ]
 
 
+        zone = self.df.iloc[
+            origin_index:
+            bos_index + 1
+        ]
+
+
         block = OrderBlock(
 
             direction="Bearish",
